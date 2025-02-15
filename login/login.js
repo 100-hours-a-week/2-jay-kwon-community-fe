8dd00fcb3a2ec2315080f8f8d4602482aa769892
DEOKJAE KWON <juintin@kakao.com>
2025-02-16 00:40:53 +0900

    feat: add an effect of a login button

--- login/login.js
+++ login/login.js
@@ -25,7 +25,14 @@ document.addEventListener("DOMContentLoaded", function () {
                     const user = users.find(user => user.email === emailValue && user.password === passwordValue);
                     if (user) {
                         helperText.style.display = "none";
-                        window.location.href = "posts.html";
+                        const originalColor = loginButton.style.backgroundColor;
+                        loginButton.style.backgroundColor = "#7F6AEE";
+                        setTimeout(() => {
+                            loginButton.style.backgroundColor = originalColor; // 원래 색상으로 되돌림
+                            setTimeout(() => {
+                                window.location.href = "posts.html"; // 페이지 이동
+                            }, 500); // 0.5초 후 페이지 이동
+                        }, 500); // 0.5초 동안 색상 유지
                     } else {
                         helperText.textContent = "*아이디 또는 비밀번호를 확인해주세요";
                         helperText.style.display = "block";
