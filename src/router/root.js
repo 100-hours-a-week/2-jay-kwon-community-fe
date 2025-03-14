25ad2e19c879d2ff7a42934ba5360706a66b326f
DEOKJAE KWON <juintin@kakao.com>
2025-03-14 17:42:56 +0900

    feat: add root router

---
+++ src/router/root.js
@@ -0,0 +1,10 @@
+import{ createBrowserRouter } from 'react-router-dom'
+
+const root = createBrowserRouter([
+    {
+        path: '',
+        element: <div>Main Page</div>
+    }
+])
+
+export default root
