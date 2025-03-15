63a062db175816f9b7b548e4a45044245cd2e1c2
DEOKJAE KWON <juintin@kakao.com>
2025-03-16 00:09:13 +0900

    refactor: modify MainComponent for redirection

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import useCustomLogin from "../hooks/useCustomLogin";
 
 function MainComponent(props) {
+    const navigate = useNavigate();
+    const { isLogin } = useCustomLogin();
+
+    useEffect(() => {
+        if (isLogin) {
+            navigate("/posts/list");
+        } else {
+            navigate("/users/login");
+        }
+    }, [isLogin, navigate]);
+
     return (
         <div className="bg-[#F4F5F7] min-h-screen"></div>
     );
