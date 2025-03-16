e84dff548c6c63556e061c021fc70d348ac4754f
DEOKJAE KWON <juintin@kakao.com>
2025-03-16 16:40:07 +0900

    refactor: modify BasicLayout to make BasicMenu fixed at the top

--- src/layouts/BasicLayout.js
+++ src/layouts/BasicLayout.js
@@ -4,7 +4,9 @@ import BasicMenu from '../components/menus/BasicMenu';
 const BasicLayout = ({ children }) => {
     return (
         <>
-            <BasicMenu />
+            <div className="fixed top-0 left-0 w-full z-50">
+                <BasicMenu />
+            </div>
             {children}
         </>
     );
